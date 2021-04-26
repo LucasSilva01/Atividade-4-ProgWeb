0c3b5ac839a15a77fa94781b83c94d207e940a29
LucasSilva01 <santoslucas9956@gmail.com>
2021-04-26 20:15:13 -0300

    index

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
+
+
+
+
+const app = express();
+
+//permite dados do tipo json
+app.use(express.json());
+app.use(cors());
+
+//evitar problema de compatibiladde
+mongoose.Promise = global.Promise;
+mongoose.createConnection('mongodb://localhost:27017/curso',{
+    useNewUrlParser:true, useUnifiedTopology: true
+}).then(() =>{ 
+    console.log("Conectado ao mongo!")
+}).catch((err) => {
+    console.log("Erro: "+err)
+})
+
+requireDir('./src/models')
+app.use('/sistema', require('./src/routes/routes'));
+
+
+const PORT = 3005
+
+app.listen(PORT, () => {
+    requireDir('./src/models')
+    app.use('/sistema', require('./src/routes/routes'))
+    console.log("Servidor Rodando")
+})
+
