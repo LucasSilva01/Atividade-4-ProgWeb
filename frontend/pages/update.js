bfbc7a43f1427c04eead63035654b070af5c2190
danrleydaniel <danrleydaniel21@gmail.com>
2021-04-29 22:55:31 -0300

    update page

---
+++ frontend/pages/update.js
@@ -0,0 +1,97 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import api from '../services/services';
+import User from '../components/User'
+
+const AtualizaCarros extends Components {
+
+	/*
+	INFELIZMENTE NÃO CONSEGUI IMPLEMENTAR O NECESSÁRIO PARA REALIZAR A EDIÇÃO
+	DO CARRO, ENTÃO FIZ APENAS A PÁGINA DE EDIÇÃO.
+	*/
+    return(
+    	<div>
+    		<form onSubmit={this.handleSubmit}>
+    			<legend> Editar carro </legend>
+
+    			<div>
+    				<label htmlFor="placa">Digite a placa do carro: </label>
+                            <br />
+                            <input
+                                type="text"
+                                id="placa"
+                                name="placa"
+                                placeholder="Placa"
+
+
+                                value={this.state.carro.placa}
+                                onChange={this.handleInputChange}
+                            />
+    			</div>
+
+    				<div>
+    					Digite as novas informações:
+    				</div>	
+
+    				<div>
+                            <label htmlFor="marca">Marca </label>
+                            <br />
+                            <input
+                                type="text"
+                                id="marca"
+                                name="marca"
+                                placeholder="Marca"
+                                value={this.state.carro.marca}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+                        <div >
+                            <label htmlFor="placa">Placa </label>
+                            <br />
+                            <input
+                                type="text"
+                                id="placa"
+                                name="placa"
+                                placeholder="Placa"
+
+
+                                value={this.state.carro.placa}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+                        <div >
+                            <label htmlFor="renavan">renavan </label>
+                            <br />
+                            <input
+                                type="text"
+                                id="renavan"
+                                name="renavan"
+                                placeholder="renavan"
+
+
+                                value={this.state.carro.renavan}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+                        <div >
+                            <label htmlFor="cor">cor </label>
+                            <br />
+                            <input
+                                type="text"
+                                id="cor"
+                                name="cor"
+                                placeholder="cor"
+
+
+                                value={this.state.carro.cor}
+                                onChange={this.handleInputChange}
+                            />
+                        </div>
+                    <button type="submit" className="btn btn-primary">
+                        Cadastrar
+                    </button>
+            </form>
+    	</div>
+    )
+}
+
+export default AtualizaCarros;
